1965ba1726c57df6475d8cf90f9c942d35a88639
DiegoAndre Leffa <diegodocanto@gmail.com>
2023-06-13 14:25:33 -0300

    feat: Create tables imgCoven/gallery and fix create/get/update announ… 36ad512 git commit -m

--- src/services/announcement/updateAnnouncement.service.ts
+++ src/services/announcement/updateAnnouncement.service.ts
@@ -1,35 +1,3 @@
-// import { Repository } from "typeorm";
-// import { IReturnAnnouncement, IUpdateAnnouncement } from "../../interfaces";
-// import { Announcement } from "../../entities";
-// import { AppDataSource } from "../../data-source";
-// import { returnAnnouncementSchema } from "../../schemas";
-
-// export const updateAnnouncementService = async (
-//   data: IUpdateAnnouncement,
-//   idAnnouncement: string,
-//   imgCoverFile: Express.Multer.File,
-//   galleryFiles: Express.Multer.File[]
-// ): Promise<IReturnAnnouncement> => {
-//   const announcementRepository: Repository<Announcement> =
-//     AppDataSource.getRepository(Announcement);
-
-//   const oldAnnouncementData: Announcement | null =
-//     await announcementRepository.findOneBy({
-//       id: idAnnouncement,
-//     });
-
-//   const newDataAnnouncement = announcementRepository.create({
-//     ...oldAnnouncementData,
-//     ...data,
-//   });
-
-//   await announcementRepository.save(newDataAnnouncement);
-
-//   const announcement = returnAnnouncementSchema.parse(newDataAnnouncement);
-
-//   return announcement;
-// };
-
 import { Repository } from "typeorm";
 import { IReturnAnnouncement, IUpdateAnnouncement } from "../../interfaces";
 import { Announcement, ImgCover, Gallery } from "../../entities";
